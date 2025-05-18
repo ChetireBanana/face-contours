d7066b7789af534a0f877d9e935ca298dab78da1
Sergey <mr.veretennikov.s@gmail.ru>
2025-05-18 19:02:45 +0300

    Face analyzer added

---
+++ app/src/main/java/com/example/facecontours/data/local/camera/ImageAnalyzer.kt
@@ -0,0 +1,43 @@
+package com.example.facecontours.data.local.camera
+
+import androidx.annotation.OptIn
+import androidx.camera.core.ExperimentalGetImage
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.common.InputImage
+import com.google.mlkit.vision.face.Face
+import com.google.mlkit.vision.face.FaceDetection
+import com.google.mlkit.vision.face.FaceDetectorOptions
+
+class ImageAnalyzer (
+    private val onFaceDetected: (List<Face>) -> Unit
+) : ImageAnalysis.Analyzer {
+
+    private val options = FaceDetectorOptions.Builder()
+        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_FAST)
+        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
+        .build()
+
+    private val detector = FaceDetection.getClient(options)
+
+    @OptIn(ExperimentalGetImage::class)
+    override fun analyze(imageProxy: ImageProxy) {
+        val mediaImage = imageProxy.image
+        if (mediaImage != null) {
+            val image = InputImage.fromMediaImage(mediaImage, imageProxy.imageInfo.rotationDegrees)
+
+            detector.process(image)
+                .addOnSuccessListener { faces ->
+                    onFaceDetected(faces)
+                }
+                .addOnFailureListener {
+                   onFaceDetected(emptyList())
+                }
+                .addOnCompleteListener {
+                    imageProxy.close()
+                }
+        } else {
+            imageProxy.close()
+        }
+    }
+}
